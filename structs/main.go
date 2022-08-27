6acccd3abe1d2cb207f1b94b363be6238c396071
kitar-kks <kroekkiat.kitar@gmail.com>
2022-08-27 08:35:09 +0700

    struct pointer reciever

---
+++ structs/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	// var alex person
+	// // alex := person{firstName: "Alex", lastName: "Anderson"}
+	// alex.firstName = "Subaru"
+	// alex.lastName = "just a name"
+	// // fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "Jim@gmail.com",
+			zipCode: 12102,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	// jim.updateName("Jimmy")
+	jim.print()
+}
+
+// func (p person) updateName(newFirstName string) {
+// 	p.firstName = newFirstName // when we call p is just copy jim to other address in ram so we need to use pointer to fix that
+// }
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	pointerToPerson.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
