14999cb5241989b258cf45987f3dd541b56c48fe
kitar-kks <kroekkiat.kitar@gmail.com>
2022-08-27 14:07:30 +0700

    manipulate map

--- map/main.go
+++ map/main.go
@@ -1,16 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // map like objects in js , dict in python
 func main() {
-	var colors map[string]string
+	// var colors map[string]string
+	// colors := make(map[string]string)
+	// map[type of key]type of value
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "ffffff",
+	}
 
-	fmt.Println(colors)
+	// colors["white"] = "#fffffff"
+	// fmt.Println(colors)
+	// delete(colors, "white")
+	// fmt.Println(colors)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println(key, value)
+	}
 }
